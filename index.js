ae11903dfb0fcc72cff51205873386e3e4413ec3
Ishaan Rao <ishaan_rao@icloud.com>
2023-07-11 18:16:42 +0530

    Added login button effects, added login page, added favicon in files, and fixed margin resizing errors by using percentages instead of pixels

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 // Startup script to make sure the code is running
 console.log('Code works')
+
+// Login button script
+function loginbutton() {
+    window.alert("Login page under maintenance")
+}
+
+function loginpage() {
+    window.location.href = "loginpage.html";
+  }
